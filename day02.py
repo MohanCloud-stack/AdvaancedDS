5b2a70478bdfb3bf649f159423c496ee340fa967
bmohan048@gmail.com <bmohan048@gmail.com>
2023-05-09 18:32:56 +0530

    arange addition

---
+++ day02.py
@@ -0,0 +1,10 @@
+import numpy as np
+# print(help(np.array))
+# print(np.array([1,2,3.0]))
+# print(np.array([1,2,3],dtype=complex))
+# print(np.array([[1,2],[3,4]]))
+# print(np.array((1,2,3,4,5)))
+print(np.arange(1,10)) # step is 1
+print(np.arange(3.0)) #floating point values
+print(np.arange(1,10,2)) #step is 2
+print(np.arange(1,20,dtype="complex")) #mentioning the data type
