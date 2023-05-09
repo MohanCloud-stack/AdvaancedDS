35b8a43dd9b5e15aba8857cf11912be4f2d74317
bmohan048@gmail.com <bmohan048@gmail.com>
2023-05-09 19:24:13 +0530

    added the linspace

---
+++ Linspace.py
@@ -0,0 +1,6 @@
+import numpy as np 
+print(np.linspace(2.0,3.0,num=5))
+print(np.linspace(2.0,3.0,num=5,endpoint=False))
+print(np.linspace(1,100,num=5,retstep=True,endpoint=False))
+print(np.linspace(1,100,num=5,retstep=True))
+print(np.linspace(1,100,num=4,dtype=int))
