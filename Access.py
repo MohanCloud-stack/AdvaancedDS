e21b361b4a7d0d512824bba1a46ca164e8330823
bmohan048@gmail.com <bmohan048@gmail.com>
2023-05-09 19:01:12 +0530

    added Zeros.py

---
+++ Access.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr=np.array([[1,2,3,4],[6,7,8,9]])
+print(arr[0,1])
+print(arr[1,3])
+#negative indexing
+print(arr[1,-1])
+print(arr.dtype)
---
+++ Zeros.py
@@ -0,0 +1,5 @@
+import numpy as np 
+print(np.zeros(5))
+print(np.zeros(3,dtype="int"))
+# 2dimensional Array
+print(np.zeros((2,3)))# 2 rows 3 columns
