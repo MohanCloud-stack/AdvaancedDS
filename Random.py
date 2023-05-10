9aafbb12042bac7fb128c4342abe4233d8ae18d3
bmohan048@gmail.com <bmohan048@gmail.com>
2023-05-10 18:48:27 +0530

    added the random.py

---
+++ Random.py
@@ -0,0 +1,10 @@
+import numpy as np 
+# print(help(np.random.rand))
+print(np.random.rand(3,2))
+# for single dimension array
+print(np.random.rand(3))
+#Normally distributed values
+print(np.random.randn(5))
+#Uniformly Distributed floating point values
+print(np.random.ranf())
+print(np.random.random_sample(5,))
