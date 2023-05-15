0b7fbdb103738cb497847295eff57207c849b4c4
bmohan048@gmail.com <bmohan048@gmail.com>
2023-05-15 17:29:33 +0530

    added the manipulate

---
+++ Manipulate.py
@@ -0,0 +1,14 @@
+import numpy as np
+# a=np.arange(10)
+# print(a.shape)
+# # row wise
+# b=np.reshape(a,(5,2))
+# print(b)
+# #column wise
+# c=np.reshape(a,(5,2),order="F")
+# print(c)
+# print(a.size)
+#resize
+a=np.arange(5)
+print(a)
+print(np.resize(a,(5,4)))
